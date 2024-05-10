9253e9ec5ae0f8ad0e344fa3b862842abe746584
dhitozarfhan <dhitozarfhan@gmail.com>
2024-05-10 09:01:27 +0700

    clas node

--- pertemuan10/pertemuan10.cpp
+++ pertemuan10/pertemuan10.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
 
+using namespace std;
+
+// node class representing a single node iin the linked list 
+class Node {
+public:
+	int data;
+	Node* next;
+
+	Node() {
+		next = NULL;
+	}
+};
+	
